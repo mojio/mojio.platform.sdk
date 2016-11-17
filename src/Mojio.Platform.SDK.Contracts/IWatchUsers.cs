5342b5d6b9ee5ad9e846a6b4ffbe76c85f1be035
Paul Bauer <paulb@moj.io>
2016-11-17 14:32:29 -0800

    moved more observer command to new file

---
+++ src/Mojio.Platform.SDK.Contracts/IWatchUsers.cs
@@ -0,0 +1,15 @@
+﻿using Mojio.Platform.SDK.Contracts.Client;
+using Mojio.Platform.SDK.Contracts.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mojio.Platform.SDK.Contracts
+{
+    public interface IWatchUsers
+    {
+        Task<IObservable<IUser>> WatchUsers(IClient client, string userId = null, CancellationToken cancellationToken = default(CancellationToken), Action<IVehicle> changedAction = null);
+    }
+}
