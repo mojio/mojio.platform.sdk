98da02443c5da12a0a98cbd6fea72e80e3d59466
Paul Bauer <paulb@moj.io>
2016-11-17 13:45:20 -0800

    added more observer deletion functionality to cli

--- src/Mojio.Platform.SDK.CLI/Commands/MojioCommands.cs
+++ src/Mojio.Platform.SDK.CLI/Commands/MojioCommands.cs
@@ -139,19 +139,111 @@ namespace Mojio.Platform.SDK.CLI.Commands
 
 #endif
 
-    [CommandDescriptor(Name = "list-watched-mojios", Description = "List currently watched mojios",
-         Usage = "list-watched-mojios")]
+    [CommandDescriptor(Name = "list-watched-mojios", Description = "List currently watched mojios", Usage = "list-watched-mojios")]
     public class ListWatchedMojiosCommand : BaseCommand
     {
-        [Argument(ArgumentType.AtMostOnce, ShortName = "id")]
-        public string Id { get; set; } = null;
-
         public override async Task Execute()
         {
             await Authorize();
 
             var result = await SimpleClient.GetObservers(ObserverEntity.Mojios);
             Log.Debug(result);
+
+            UpdateAuthorization();
+        }
+    }
+
+    [CommandDescriptor(Name = "delete-watched-mojio", Description = "Delete specified watched mojio", Usage = "delete-watched-mojio /id:<observer-id>")]
+    public class DeleteWatchedMojioCommand : BaseCommand
+    {
+        [Argument(ArgumentType.Required, ShortName = "i")]
+        public string Id { get; set; }
+
+        public override async Task Execute()
+        {
+            await Authorize();
+
+            var result = await SimpleClient.DeleteObserver(ObserverEntity.Mojios, Id);
+            Log.Debug(result);
+
+            UpdateAuthorization();
+        }
+    }
+
+    [CommandDescriptor(Name = "delete-watched-mojios", Description = "Deletes all watched mojios", Usage = "delete-watched-mojios")]
+    public class DeleteWatchedMojiosCommand : BaseCommand
+    {
+        public override async Task Execute()
+        {
+            await Authorize();
+
+            var listResult = await SimpleClient.GetObservers(ObserverEntity.Mojios);
+            if (!listResult.Success || listResult.Response == null)
+            {
+                Log.Debug(listResult);
+                return;
+            }
+
+            foreach (var watched in listResult.Response)
+            {
+                var result = await SimpleClient.DeleteObserver(ObserverEntity.Mojios, watched.Key);
+                Log.Debug(result);
+            }
+
+            UpdateAuthorization();
+        }
+    }
+
+    [CommandDescriptor(Name = "list-watched-users", Description = "List currently watched users", Usage = "list-watched-users")]
+    public class ListWatchedUsersCommand : BaseCommand
+    {
+        public override async Task Execute()
+        {
+            await Authorize();
+
+            var result = await SimpleClient.GetObservers(ObserverEntity.Users);
+            Log.Debug(result);
+
+            UpdateAuthorization();
+        }
+    }
+
+    [CommandDescriptor(Name = "delete-watched-user", Description = "Delete specified watched user", Usage = "delete-watched-user /id:<observer-id>")]
+    public class DeleteWatchedUserCommand : BaseCommand
+    {
+        [Argument(ArgumentType.Required, ShortName = "i")]
+        public string Id { get; set; }
+
+        public override async Task Execute()
+        {
+            await Authorize();
+
+            var result = await SimpleClient.DeleteObserver(ObserverEntity.Users, Id);
+            Log.Debug(result);
+
+            UpdateAuthorization();
+        }
+    }
+
+    [CommandDescriptor(Name = "delete-watched-users", Description = "Deletes all watched users", Usage = "delete-watched-users")]
+    public class DeleteWatchedUsersCommand : BaseCommand
+    {
+        public override async Task Execute()
+        {
+            await Authorize();
+
+            var listResult = await SimpleClient.GetObservers(ObserverEntity.Users);
+            if (!listResult.Success || listResult.Response == null)
+            {
+                Log.Debug(listResult);
+                return;
+            }
+
+            foreach (var watched in listResult.Response)
+            {
+                var result = await SimpleClient.DeleteObserver(ObserverEntity.Users, watched.Key);
+                Log.Debug(result);
+            }
 
             UpdateAuthorization();
         }
