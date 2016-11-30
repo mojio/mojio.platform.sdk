be37b3b0266cc7918b904454252861d7c6b6cc9a
Paul Bauer <paulb@moj.io>
2016-11-29 18:25:44 -0800

    added a get-user CLI function

---
+++ src/Mojio.Platform.SDK.CLI/Commands/UserCommands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Mojio.Platform.SDK.CLI.Commands
+{
+    [CommandDescriptor(Name = "get-user", Description = "Gets the specified user", Usage = "get-user  /id:<user-id>")]
+    public class GetUserCommand : BaseCommand
+    {
+        [Argument(ArgumentType.Required, ShortName = "i")]
+        public string Id { get; set; }
+
+        public override async Task Execute()
+        {
+            await Authorize();
+            var g = Guid.Empty;
+            if (Guid.TryParse(Id, out g))
+            {
+                var result = await SimpleClient.GetUser(g);
+                Log.Debug(result);
+                UpdateAuthorization();
+            }
+            else
+            {
+                Log.Info("Invalid Id specified.");
+            }
+        }
+    }
+}
