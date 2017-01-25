2f71c91f9acf633071c040ed022c18553bfe6749
Ashish Agarwal <iamashish.agarwal@gmail.com>
2017-01-25 14:29:23 -0800

    Completed the authentication tests

---
+++ test/mojio.platform.sdk.test/AuthenticationTests.cs
@@ -0,0 +1,122 @@
+﻿using System.Threading.Tasks;
+using Mojio.Platform.SDK.Contracts.Client;
+using Mojio.Platform.SDK.Contracts.Entities;
+using Mojio.Platform.SDK.SimpleClient;
+using Mojio.Platform.SDK.Tests;
+using Xunit;
+
+namespace mojio.platform.sdk.test
+{
+    public class AuthenticationTests
+    {
+        private readonly IClient _client;
+        
+        public AuthenticationTests()
+        {
+            _client = new SimpleClient(Mother.Environment, new Configuration
+            {
+                ClientId = Mother.ClientId,
+                ClientSecret = Mother.ClientSecret,
+                RedirectUri = Mother.RedirectUri
+            });
+        }
+
+        [Fact]
+        [Trait("Category", "Functional")]
+        [Trait("Category", "UnitTest")]
+        public async Task LoginUsernameSpecialCasePasswordTestAsync()
+        {
+            var credentials = Mother.CredentialsWithSpecialCasePassword;
+            var response = await _client.Login(credentials.Item1, credentials.Item3);
+
+            Assert.NotNull(response.Response?.MojioApiToken);
+        }
+
+        [Fact]
+        [Trait("Category", "Functional")]
+        [Trait("Category", "UnitTest")]
+        public async Task LoginUsernameWithIncorrectPasswordTestAsync()
+        {
+            var credentials = Mother.Credentials;
+            var response = await _client.Login(credentials.Item1, "ad*afxf.a(;");
+            Assert.Null(response.Response?.MojioApiToken);
+        }
+
+        [Fact]
+        [Trait("Category", "Functional")]
+        [Trait("Category", "UnitTest")]
+        public async Task LoginUsernameWithEmptyPasswordTestAsync()
+        {
+            var credentials = Mother.Credentials;
+            var response = await _client.Login(credentials.Item1, "");
+            Assert.Null(response.Response?.MojioApiToken);
+        }
+
+        [Fact]
+        [Trait("Category", "Functional")]
+        [Trait("Category", "UnitTest")]
+        public async Task LoginEmailTestAsync()
+        {
+            var credentials = Mother.Credentials;
+            var response = await _client.Login(credentials.Item2, credentials.Item3);
+            Assert.NotNull(response.Response?.MojioApiToken);
+        }
+
+        [Fact]
+        [Trait("Category", "Functional")]
+        [Trait("Category", "UnitTest")]
+        public async Task LoginEmailWithSpecialCasePasswordTestAsync()
+        {
+            var credentials = Mother.CredentialsWithSpecialCasePassword;
+            var response = await _client.Login(credentials.Item2, credentials.Item3);
+            Assert.NotNull(response.Response?.MojioApiToken);
+        }
+
+        [Fact]
+        [Trait("Category", "Functional")]
+        [Trait("Category", "UnitTest")]
+        public async Task LoginWithAuthTokenTestAsync()
+        {
+            var credentials = Mother.Credentials;
+            var response = await _client.Login(credentials.Item1, credentials.Item3);
+
+            var responseWithToken = await _client.Login(response.Response?.MojioApiToken);
+            Assert.NotNull(responseWithToken.Response?.MojioApiToken);
+        }
+
+        [Fact]
+        [Trait("Category", "Functional")]
+        [Trait("Category", "UnitTest")]
+        public async Task LoginWithAuthorizationTestAsync()
+        {
+            var client = new SimpleClient(Mother.Environment, new Configuration
+            {
+                ClientId = Mother.ClientId,
+                ClientSecret = Mother.ClientSecret,
+                RedirectUri = Mother.RedirectUri
+            });
+
+            var credentials = Mother.Credentials;
+
+            var authorization = new CoreAuthorization
+            {
+                UserName = credentials.Item1,
+                Password = credentials.Item3
+            };
+
+            var response = await client.Login(authorization);
+            Assert.NotNull(response.Response?.MojioApiToken);
+        }
+
+        [Fact]
+        [Trait("Category", "Functional")]
+        [Trait("Category", "UnitTest")]
+        public async Task RefreshTokenTestAsync()
+        {
+            var credentials = Mother.Credentials;
+            var response = await _client.Login(credentials.Item1, credentials.Item3);
+            var refreshTokenResponse = await _client.RefreshToken();
+            Assert.NotNull(refreshTokenResponse.Response?.MojioApiToken);
+        }
+    }
+}
--- test/mojio.platform.sdk.test/Mother.cs
+++ test/mojio.platform.sdk.test/Mother.cs
@@ -1,100 +1,110 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using mojio.platform.sdk.test;
+using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
 using Mojio.Platform.SDK.Contracts;
 using Mojio.Platform.SDK.Contracts.Client;
 using Mojio.Platform.SDK.Contracts.Instrumentation;
 using Mojio.Platform.SDK.DryIoc;
 using Mojio.Platform.SDK.SimpleClient;
+using Newtonsoft.Json.Linq;
 
-namespace Mojio.Platform.SDK.Tests
+namespace mojio.platform.sdk.test
 {
     public static class Mother
     {
+        public static ILog Log;
+
+        private static readonly JObject Config;
+        public static string ClientId;
+        public static string ClientSecret;
+        public static string RedirectUri;
+        public static Environments Environment = Environments.NaStaging;
+
+        public static IDIContainer DiContainer => Mojio.Platform.SDK.Entities.DI.DIContainer.Current;
+        public static IClient GetNewSimpleClient => new SimpleClient(Environment, new Configuration { ClientId = ClientId, ClientSecret = ClientSecret, RedirectUri = RedirectUri });
+
+        public static Random Random => new Random();
+
+        public static Tuple<string, string, string> Credentials
+        {
+            get
+            {
+                var username = Config["NaStaging"]["Credentials"].First["username"].ToString();
+                var email = Config["NaStaging"]["Credentials"].First["email"].ToString();
+                var password = Config["NaStaging"]["Credentials"].First["password"].ToString();
+
+                return Tuple.Create(username, email, password);
+            }
+        }
+
+        public static Tuple<string, string, string> CredentialsWithSpecialCasePassword
+        {
+            get
+            {
+                var accounts = Config["NaStaging"]["Credentials"];
+                var regex = new Regex("[!+* /]");
+
+                foreach (var account in accounts)
+                {
+                    var username = account["username"].ToString();
+                    var email = account["email"].ToString();
+                    var password = account["password"].ToString();
+
+                    if (regex.IsMatch(password))
+                    {
+                        return Tuple.Create(username, email, password);
+                    }
+                }
+                return Tuple.Create("", "", "");
+            }
+        }
+
         static Mother()
         {
             var configBuilder = new ConfigurationBuilder();
             configBuilder.AddEnvironmentVariables();
 
+            var configurationRoot = configBuilder.Build();
+
             var root = SetupTestingPath(configBuilder);
+            var json = File.ReadAllText(root + "\\config.json");
 
-            //automated testing via VSO will automatically create this file
-            //which just sets the Envrionment variable
-            AddConfigFile(configBuilder, root, "appsettings.environment.json");
-            _configurationRoot = configBuilder.Build();
+            Config = JObject.Parse(json);
+
+            ClientId = Config["NaStaging"]["ClientId"].ToString();
+            ClientSecret = Config["NaStaging"]["SecretKey"].ToString();
+            RedirectUri = Config["NaStaging"]["RedirectUri"].ToString();
+
+            Mojio.Platform.SDK.Entities.DI.DIContainer.Current.Register<ILog, ConsoleLogger>("Debug");
+            Mojio.Platform.SDK.Entities.DI.DIContainer.Current.RegisterInstance(configurationRoot);
+
+            Log = Mojio.Platform.SDK.Entities.DI.DIContainer.Current.Resolve<ILog>();
+
+            Log.Debug(new { ClientId, ClientSecret, RedirectUri, Environment, Credentials.Item1, Credentials.Item3 });
             
-            //our root settings should have provided us with an Environment var in our config file, if so, lets wire it up
-            var configEnvironment = _configurationRoot["Environment"];
-            if (!string.IsNullOrEmpty(configEnvironment))
-            {
-                Environments env;
-                if (Enum.TryParse(configEnvironment, out env))
-                {
-                    Environment = env;
-                }
-            }
-
-            Console.WriteLine($"Mother:Environment:{Environment}");
-
-            //finally add our environment specific settings
-            AddConfigFile(configBuilder, root, $"appsettings.{Environment}.json");
-
-
-            _configurationRoot = configBuilder.Build();
-
-            ClientId = _configurationRoot["ClientId"];
-            if (string.IsNullOrEmpty(ClientId)) ClientId = "f1b18a19-f810-4f16-8a39-d6135f5ec052";
-            ClientSecret = _configurationRoot["ClientSecret"];
-            if (string.IsNullOrEmpty(ClientSecret)) ClientSecret = "aead4980-c966-4a26-abee-6bdb1ea23e5c";
-            RedirectUri = _configurationRoot["RedirectUri"];
-            if (string.IsNullOrEmpty(RedirectUri)) RedirectUri = "https://www.moj.io";
-
-            Entities.DI.DIContainer.Current.Register<ILog, ConsoleLogger>("Debug");
-            Entities.DI.DIContainer.Current.RegisterInstance(_configurationRoot);
-
-            Log = Entities.DI.DIContainer.Current.Resolve<ILog>();
-
-            Log.Debug(new {ClientId, ClientSecret, RedirectUri, Environment, Username, Password});
-
-
-        }
-
-        private static void AddConfigFile(ConfigurationBuilder configBuilder, string root, string name)
-        { 
-            var p = System.IO.Path.Combine(root, name);
-            if (System.IO.File.Exists(p))
-            {
-                configBuilder.AddJsonFile(path: p, optional: true);
-                Console.WriteLine("Config File Added:" + p);
-                Console.WriteLine(System.IO.File.ReadAllText(p));
-            }
         }
 
         private static string SetupTestingPath(ConfigurationBuilder configBuilder, string root = null, bool checkProjectStructure = true)
         {
             
-            var fileName = "appsettings.json";
+            var fileName = "config.json";
             if (string.IsNullOrEmpty(root))
             {
                 root = typeof(Mother).GetAssembly().CodeBase;
                 //root = System.Reflection.Assembly.GetEntryAssembly().CodeBase;
-                System.Uri uri = new Uri(root, UriKind.RelativeOrAbsolute);
-                System.IO.FileInfo fi = new FileInfo(uri.LocalPath);
+                var uri = new Uri(root, UriKind.RelativeOrAbsolute);
+                var fi = new FileInfo(uri.LocalPath);
                 root = fi.DirectoryName;
             }
 
-            var appSettingsFilePath = System.IO.Path.Combine(root, fileName);
+            var appSettingsFilePath = Path.Combine(root, fileName);
 
             Console.WriteLine("--Resolving appsettings--");
 
             Console.WriteLine($"==Checking:{appSettingsFilePath}==");
-            if (System.IO.File.Exists(appSettingsFilePath))
+            if (File.Exists(appSettingsFilePath))
             {
                 Console.WriteLine($"==IS GOOD:{root}==");
                 configBuilder.SetBasePath(root);
@@ -102,12 +112,12 @@ namespace Mojio.Platform.SDK.Tests
             }
             else
             {
-                System.IO.DirectoryInfo rootDirectoryInfo = new DirectoryInfo(root);
+                var rootDirectoryInfo = new DirectoryInfo(root);
                 root = rootDirectoryInfo.Parent.FullName;
-                appSettingsFilePath = System.IO.Path.Combine(root, fileName);
+                appSettingsFilePath = Path.Combine(root, fileName);
 
                 Console.WriteLine($"==Checking:{appSettingsFilePath}==");
-                if (System.IO.File.Exists(appSettingsFilePath))
+                if (File.Exists(appSettingsFilePath))
                 {
                     Console.WriteLine($"==IS GOOD:{root}==");
                     configBuilder.SetBasePath(root);
@@ -118,10 +128,10 @@ namespace Mojio.Platform.SDK.Tests
                     root = rootDirectoryInfo.GetDirectories()?.FirstOrDefault()?.FullName;
                     if (root != null)
                     {
-                        appSettingsFilePath = System.IO.Path.Combine(root, fileName);
+                        appSettingsFilePath = Path.Combine(root, fileName);
                     }
                     Console.WriteLine($"==Checking:{appSettingsFilePath}==");
-                    if (System.IO.File.Exists(appSettingsFilePath))
+                    if (File.Exists(appSettingsFilePath))
                     {
                         Console.WriteLine($"==IS GOOD:{root}==");
                         configBuilder.SetBasePath(root);
@@ -131,7 +141,7 @@ namespace Mojio.Platform.SDK.Tests
                     {
                         if (checkProjectStructure)
                         {
-                            root = System.IO.Path.GetFullPath(".");
+                            root = Path.GetFullPath(".");
                             return SetupTestingPath(configBuilder, root, false);
                         }
                         else
@@ -140,73 +150,6 @@ namespace Mojio.Platform.SDK.Tests
                         }
                     }
                 }
-            }
-        }
-
-        public static ILog Log = null;
-
-        private static string ClientId;
-        private static string ClientSecret;
-        private static string RedirectUri;
-        private static Environments Environment = Environments.Production;
-
-        public static IDIContainer DIContainer => Mojio.Platform.SDK.Entities.DI.DIContainer.Current;
-
-        public static System.Random Random => new System.Random();
-
-        private static int RandomId = Random.Next(0, 999);
-
-        public static string Username
-        {
-            get
-            {
-                var user = _configurationRoot["test_username"];
-                if (string.IsNullOrEmpty(user))
-                {
-                    if (Environment == Environments.Load)
-                    {
-                        user = "loadtest" + RandomId;
-                    }
-                    else
-                    {
-                        user = "robc@moj.io";
-                    }
-                }
-                return user;
-            }
-        }
-
-        public static string Password
-        {
-            get
-            {
-                var password = _configurationRoot["test_password"];
-                if (string.IsNullOrEmpty(password))
-                {
-                    if (Environment == Environments.Load)
-                    {
-                        password = "Password1";
-
-                    }
-                    else
-                    {
-                        password = "1080HoweVehicles";
-
-                    }
-                }
-                return password;                
-            }   
-        }
-
-        private static IConfigurationRoot _configurationRoot;
-
-        public static IClient GetNewSimpleClient
-        {
-            get
-            {
-
-                return new SimpleClient.SimpleClient(Environment, new Configuration { ClientId = ClientId, ClientSecret = ClientSecret, RedirectUri = RedirectUri });
-
             }
         }
 
