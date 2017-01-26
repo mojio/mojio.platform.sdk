75726495cb36c7df567268b11649a507553ded0a
Rob Chartier <rob.chartier@gmail.com>
2017-01-26 14:47:05 -0800

    added a sleep task, just to emulate user pausing

---
+++ src/Mojio.Platform.SDK/Automation/StandardTasks/SleepTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mojio.Platform.SDK.Contracts.Client;
+
+namespace Mojio.Platform.SDK.Automation.StandardTasks
+{
+    public class SleepTask : BaseAutomationTask
+    {
+        public int Delay { get; set; } = 100;
+
+        public override async Task Execute(IClient client, IDictionary<string, string> properties)
+        {
+            await Task.Delay(Delay);
+        }
+    }
+}
